8e05f2d435fc98e74745a445b29af4296357228c
Jakub Błaszczyk <20283462+jamebs@users.noreply.github.com>
2021-03-02 19:02:26 +0000

    Implement fragments related module

---
+++ ki67/modules/fragments/__init__.py
@@ -0,0 +1,1 @@
+from .fragmentator import Fragmentator
---
+++ ki67/modules/fragments/fragmentator.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from magda.module import Module
+from magda.decorators import finalize, produce, register, accept
+
+from ki67.common import Shared
+from ki67.modules.utils.logging import with_logger
+from ki67.interfaces.slide import Slide
+from ki67.interfaces.fragments import Fragments
+
+
+@accept(Slide)
+@produce(Fragments)
+@register('Fragmentator')
+@finalize
+class Fragmentator(Module.Runtime):
+    """ Fragmentator """
+
+    @with_logger
+    def run(self, data: Module.ResultSet, **kwargs):
+        shared = Shared(**self.shared_parameters)
+        slide: Slide = data.get(Slide)
+
+        size = self.parameters['size']
+        shape = slide.image.shape
+
+        indices = [
+            (x, y)
+            for x in range(0, shape[1] - size, shared.stride)
+            for y in range(0, shape[0] - size, shared.stride)
+        ]
+
+        return Fragments(
+            uid=slide.uid,
+            fragments=pd.DataFrame([
+                self.create_fragment(x, y)
+                for x, y in indices
+            ]),
+        )
+
+    def create_fragment(self, x1, y1):
+        size = self.parameters['size']
+        y2 = y1 + size
+        x2 = x1 + size
+        return pd.Series(
+            data=dict(y1=y1, y2=y2, x1=x1, x2=x2),
+            name=f'{y1}_{y2}-{x1}_{x2}',
+        )
