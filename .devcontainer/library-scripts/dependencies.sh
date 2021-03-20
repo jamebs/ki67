5aef62832f07020388542a232966034b2a075a01
Jakub Błaszczyk <20283462+jamebs@users.noreply.github.com>
2021-03-20 12:51:02 +0000

    Environment update

--- .devcontainer/library-scripts/dependencies.sh
+++ .devcontainer/library-scripts/dependencies.sh
@@ -11,6 +11,7 @@ conda install -yq \
     'dask=2021.2.*' \
     'fastparquet=0.5.*' \
     'ipykernel=5.4.*' \
+    'ipywidgets=7.6.*' \
     'matplotlib-base=3.3.*' \
     'numba=0.52.*' \
     'numpy=1.19.*' \
@@ -22,7 +23,7 @@ conda install -yq \
     'scikit-learn=0.24.*' \
     'seaborn=0.11.*' \
     'scipy=1.6.*' \
-    'tqdm=4.56.*'
+    'tqdm=4.59.*'
 
 echo "Installing Tensorflow..."
 pip install --quiet --no-cache-dir tensorflow==2.4
